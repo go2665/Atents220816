1f43d33b98d6e03ede33d436cfc38dfa8c9888fb
go2665 <77224762+go2665@users.noreply.github.com>
2022-08-18 12:59:08 +0900

    Console - 클래스 생성자

--- 01_Console/01_Console/Test.cs
+++ 01_Console/01_Console/Test.cs
@@ -25,10 +25,35 @@ namespace _01_Console
         private int dexterity = 5;
         private int intellegence = 7;
 
+        //Random random = new Random();
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    int randNum = random.Next();
+        //    // % : 앞에 숫자를 뒤의 숫자로 나눈 나머지값을 돌려주는 연산자. (모듈레이트 연산. 나머지 연산)
+        //    // 10 % 3 하면 결과는 1
+        //    // % 연산의 결과는 항상 0~(뒤 숫자-1)로 나온다.
+        //    Console.WriteLine($"랜덤 넘버 : {randNum}");
+        //}
+
+
+        // 배열 : 같은 종류(데이터타입)의 데이터를 한번에 여러개 가지는 유형의 변수
+        //int[] intArray; // 인티저를 여러개 가질 수 있는 배열
+        //intArray = new int[5];    // 인티저를 5개 가질 수 있도록 할당
+
+        string[] nameArray = { "너굴맨", "개굴맨", "ㅁㅁㅁ", "ㄷㄷㄷ", "ㅋㅋㅋ" }; // nameArray에 기본값 설정(선언과 할당을 동시에 처리)
+
         public Character()
         {
             Console.WriteLine("생성자 호출");
-            name = "너굴맨";
+
+            // 실습
+            // 1. 이름이 nameArray에 들어있는 것 중 하나로 랜덤하게 선택된다.
+            // 2. maxHP는 100~200 사이로 랜덤하게 선택된다.
+            // 3. hp는 maxHP와 같은 값이다.
+            // 4. strenth, dexterity, intellegence은 1~20 사이로 랜덤하게 정해진다.
+            // 5. TestPrintStatus 함수를 이용해서 최종 상태를 출력한다.
+            // 시간 : 1시 20분
+
         }
 
         public Character(string newName)
@@ -54,8 +79,7 @@ namespace _01_Console
         }
 
         public void TestPrintStatus()
-        {
-
+        {            
         }
     }
 }
