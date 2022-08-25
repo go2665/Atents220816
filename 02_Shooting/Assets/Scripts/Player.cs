7c735988c3496d6febff69dc7001ef7a749c6b79
go2665 <77224762+go2665@users.noreply.github.com>
2022-08-25 12:43:13 +0900

    Shooting - 플레이어 이동

--- 02_Shooting/Assets/Scripts/Player.cs
+++ 02_Shooting/Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 1.0f;      // 플레이어의 이동 속도(초당 이동 속도)
+    Vector3 dir;                    // 이동 방향(입력에 따라 변경됨)
+
     PlayerInputAction inputActions;
     // Awake -> OnEnable -> Start : 대체적으로 이 순서
 
@@ -23,15 +26,17 @@ public class Player : MonoBehaviour
     private void OnEnable()
     {
         inputActions.Player.Enable();   // 오브젝트가 생성되면 입력을 받도록 활성화
-        inputActions.Player.Move.performed += OnMove;   // performed 일 때 OnMove 함수 실행하도록 연결
-        inputActions.Player.Move.canceled += OnMove;    // canceled 일 때 OnMove 함수 실행하도록 연결
-    }    
+        inputActions.Player.Move.performed += OnMove;   // Move액션이 performed 일 때 OnMove 함수 실행하도록 연결
+        inputActions.Player.Move.canceled += OnMove;    // Move액션이 canceled 일 때 OnMove 함수 실행하도록 연결
+        inputActions.Player.Fire.performed += OnFire;
+    }
 
     /// <summary>
     /// 이 스크립트가 들어있는 게임 오브젝트가 비활성화 되었을 때 호출
     /// </summary>
     private void OnDisable()
     {
+        inputActions.Player.Fire.performed -= OnFire;
         inputActions.Player.Move.canceled -= OnMove;    // 연결해 놓은 함수 해제(안전을 위해)
         inputActions.Player.Move.performed -= OnMove;
         inputActions.Player.Disable();  // 오브젝트가 사라질때 더 이상 입력을 받지 않도록 비활성화
@@ -42,7 +47,7 @@ public class Player : MonoBehaviour
     /// </summary>
     private void Start()
     {
-        
+
     }
 
     /// <summary>
@@ -50,7 +55,7 @@ public class Player : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        
+        transform.position += (speed * Time.deltaTime * dir);
     }
 
     private void OnMove(InputAction.CallbackContext context)
@@ -58,6 +63,13 @@ public class Player : MonoBehaviour
         // Exception : 예외 상황( 무엇을 해야 할지 지정이 안되어있는 예외 일때 )
         //throw new NotImplementedException();    // NotImplementedException 을 실행해라. => 코드 구현을 알려주기 위해 강제로 죽이는 코드
 
-        Debug.Log("이동 입력");
+        //Debug.Log("이동 입력");
+        Vector2 inputDir = context.ReadValue<Vector2>();    // 어느 방향으로 움직여야 하는지를 입력받음
+        dir = inputDir;
+    }
+
+    private void OnFire(InputAction.CallbackContext context)
+    {
+        Debug.Log("발사!");
     }
 }
