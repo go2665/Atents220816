3d8b8811fa8e6a00f567016f3b757d103c06d8ec
go2665 <77224762+go2665@users.noreply.github.com>
2022-08-29 13:18:32 +0900

    Shooting -적 생성 스포너 작성 중

---
+++ 02_Shooting/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 1. EnemySpawner.cs를 수정하여 Enemy 프리팹을 지속적으로 생성하게 만들어보기.
+//  1.1.반드시 코루틴으로 작성해야 한다.
+
+public class EnemySpawner : MonoBehaviour
+{
+    // 필요한 변수가 무엇인가? -> Enemy 프리팹, 지속적으로 동작을 하는 시간 간격
+    
+    public GameObject enemy;        // 생성할 적의 프리팹
+    public float interval = 0.5f;   // 생성할 시간 간격
+
+    float minY = -4.5f;     // 스폰이 일어나는 최저 높이
+    float maxY = 4.5f;      // 스폰이 일어나는 최고 높이
+
+
+    // 필요한 기능은 무엇인가? -> Enemy 프리팹을 생성하는 코루틴
+    private void Start()
+    {
+        StartCoroutine(Spawn());    // 코루틴 시작(종료는 없음)
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)    // 무한 반복
+        {
+            GameObject obj = Instantiate(enemy, transform);  // 생성하고 부모를 이 오브젝트로 설정
+            obj.transform.Translate(0, Random.Range(minY, maxY), 0);    // 스폰 생성 범위 안에서 랜덤으로 높이 정하기
+            yield return new WaitForSeconds(interval);  // interval만큼 대기
+        }
+    }
+
+}
