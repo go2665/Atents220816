fee1df8b62da4febca186a8b14d237aeabf43af3
go2665 <77224762+go2665@users.noreply.github.com>
2022-09-06 11:51:59 +0900

    Shooting - 배경 스크롤링(작은별) 추가

--- 02_Shooting/Assets/Scripts/Background.cs
+++ 02_Shooting/Assets/Scripts/Background.cs
@@ -9,7 +9,7 @@ public class Background : MonoBehaviour
 
     const float Background_Width = 13.6f;
 
-    private void Awake()
+    protected virtual void Awake()
     {
         bgSlots = new Transform[transform.childCount];
         for( int i=0; i<transform.childCount; i++)  // 정확한 인덱스가 필요할 때 유리
@@ -21,16 +21,31 @@ public class Background : MonoBehaviour
     private void Update()
     {
         float minusX = transform.position.x - Background_Width;
-        foreach (Transform slot in bgSlots)  // 속도가 그냥 for보다 빠름
-        {
-            slot.Translate(scrollingSpeed * Time.deltaTime * -transform.right);
+        //foreach (Transform slot in bgSlots)  // 속도가 그냥 for보다 빠름
+        //{
+        //    slot.Translate(scrollingSpeed * Time.deltaTime * -transform.right);
+        //    if( slot.position.x < minusX )  
+        //    {
+        //        //Debug.Log($"{slot.name}은 충분히 왼쪽으로 이동했다.");
+        //        // 오른쪽으로 Background_Width의 3배(bgSlots.Length에 3개가 들어있으니까) 만큼 이동
+        //        MoveRightEnd(slot);
+        //    }
+        //}
 
-            if( slot.position.x < minusX )  
+        for( int i=0; i<bgSlots.Length; i++)
+        {
+            bgSlots[i].Translate(scrollingSpeed * Time.deltaTime * -transform.right);
+            if (bgSlots[i].position.x < minusX)
             {
-                //Debug.Log($"{slot.name}은 충분히 왼쪽으로 이동했다.");
-                // 오른쪽으로 Background_Width의 3배(bgSlots.Length에 3개가 들어있으니까) 만큼 이동
-                slot.Translate(Background_Width * bgSlots.Length * transform.right);    
+                //Debug.Log($"{slot.name}은 충분히 왼쪽으로 이동했다.");                
+                MoveRightEnd(i);
             }
         }
+    }
+
+    protected virtual void MoveRightEnd( int index )
+    {
+        // 오른쪽으로 Background_Width의 3배(bgSlots.Length에 3개가 들어있으니까) 만큼 이동
+        bgSlots[index].Translate(Background_Width * bgSlots.Length * transform.right);
     }
 }
---
+++ 02_Shooting/Assets/Scripts/Background_Stars.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_Stars : Background
+{
+    SpriteRenderer[] spriteRenderers;
+
+    protected override void Awake()
+    {
+        base.Awake();   // Background.Awake 실행
+
+        // 스프라이트 랜더러 미리 찾아놓기
+        spriteRenderers = new SpriteRenderer[transform.childCount];
+        for(int i=0; i<transform.childCount; i++)
+        {
+            spriteRenderers[i] = transform.GetChild(i).GetComponent<SpriteRenderer>();
+        }
+    }
+
+    protected override void MoveRightEnd(int index)
+    {
+        base.MoveRightEnd(index);
+
+        int rand = Random.Range(0, 4);
+        spriteRenderers[index].flipX = ((rand & 0b_01) != 0);
+        spriteRenderers[index].flipY = ((rand & 0b_10) != 0);
+    }
+}
