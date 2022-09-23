1af6bf07d7545774d6ab431dc0710a044b96bff8
go2665 <77224762+go2665@users.noreply.github.com>
2022-09-23 10:16:32 +0900

    3D_Basic - 플레이어를 추적하는 포탑(회전 구현)

--- 03_3D_Basic/Assets/Scripts/Turret/Turret.cs
+++ 03_3D_Basic/Assets/Scripts/Turret/Turret.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
     // turnSpeed가 아무런 영향을 주지 않는다. (총구가 즉시 회전한다.)
 
-    public float turnSpeed = 360.0f;
+    public float turnSpeed = 2.0f;
     public float sightRadius = 5.0f;
 
     Transform target = null;
     Transform barrelBody;
+
+    float currentAngle = 0.0f;
+    float targetAngle = 0.0f;
+    Vector3 initialForward;
 
     private void Awake()
     {
@@ -19,6 +24,7 @@ public class Turret : MonoBehaviour
 
     private void Start()
     {
+        initialForward = transform.forward;
         SphereCollider col = GetComponent<SphereCollider>();
         col.radius = sightRadius;
     }
@@ -29,19 +35,45 @@ public class Turret : MonoBehaviour
     private void OnValidate()
     {
         SphereCollider col = GetComponent<SphereCollider>();
-        col.radius = sightRadius;
+        if (col != null)
+        {
+            col.radius = sightRadius;
+        }
     }
 
     private void Update()
     {
         if (target != null)
         {
-            // 보간을 어떻게 적용할 것인가?
+            // 보간을 사용한 경우
+            //Vector3 dir = target.position - barrelBody.position;    // 총구에서 플레이어의 위치로 가는 방향 벡터 계산
+            //dir.y = 0;      // 방향 벡터에서 y축의 영향을 제거 => xz 평면상의 방향만 남음
 
-            // 총구를 플레이어쪽으로 돌려야 함
+            //// turnSpeed초에 걸처서 0->1로 변경된다. (시작점에서 도착점까지 걸리는 시간이 trunSpeed초)
+            //dir = Vector3.Lerp(barrelBody.forward, dir, turnSpeed * Time.deltaTime);    
+
+            //barrelBody.rotation = Quaternion.LookRotation(dir);     // 최종적인 방향을 바라보는 회전을 만들어서 총몸에 적용
+
+
+            // 각도를 사용하는 경우
             Vector3 dir = target.position - barrelBody.position;    // 총구에서 플레이어의 위치로 가는 방향 벡터 계산
-            dir.y = 0;      // 방향 벡터에서 y축의 영향을 제거 => xz 평면상의 방향만 남음
-            barrelBody.rotation = Quaternion.LookRotation(dir);     // 최종적인 방향을 바라보는 회전을 만들어서 총몸에 적용
+            dir.y = 0;
+
+            targetAngle = Vector3.SignedAngle(initialForward, dir, barrelBody.up);
+
+            if( currentAngle < targetAngle )
+            {
+                currentAngle += (turnSpeed * Time.deltaTime);
+                currentAngle = Mathf.Min(currentAngle, targetAngle);
+            }
+            else if( currentAngle > targetAngle)
+            {
+                currentAngle -= (turnSpeed * Time.deltaTime);
+                currentAngle = Mathf.Max(currentAngle, targetAngle);
+            }
+
+            Vector3 targetDir = Quaternion.Euler(0, currentAngle, 0) * initialForward;
+            barrelBody.rotation = Quaternion.LookRotation(targetDir);
         }
     }
 
