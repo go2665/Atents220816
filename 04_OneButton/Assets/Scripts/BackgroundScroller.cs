b49803a5a9df6f6ee175e03a233f79b747174983
go2665 <77224762+go2665@users.noreply.github.com>
2022-09-29 14:17:31 +0900

    OneButton - 배경 스크롤링 구현 중

---
+++ 04_OneButton/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public float scrollingSpeed = 5.0f;
+    public float width = 7.2f;  // 하늘은 7.2, 바닥은 8.4
+}
