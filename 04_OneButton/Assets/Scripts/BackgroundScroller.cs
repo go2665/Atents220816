2d86af478f555a74a9277af26af47d683163e35b
go2665 <77224762+go2665@users.noreply.github.com>
2022-09-30 09:29:50 +0900

    OneButton - 배경 스크롤링 완료

--- 04_OneButton/Assets/Scripts/BackgroundScroller.cs
+++ 04_OneButton/Assets/Scripts/BackgroundScroller.cs
@@ -6,4 +6,33 @@ public class BackgroundScroller : MonoBehaviour
 {
     public float scrollingSpeed = 5.0f;
     public float width = 7.2f;  // 하늘은 7.2, 바닥은 8.4
+
+    Transform[] bgSlots;
+    float edgePoint;
+
+    private void Awake()
+    {
+        bgSlots = new Transform[transform.childCount];
+        for(int i=0;i<transform.childCount;i++)
+        {
+            bgSlots[i] = transform.GetChild(i);
+        }
+    }
+
+    private void Start()
+    {
+        edgePoint = transform.position.x - width * 0.9f;
+    }
+
+    private void Update()
+    {        
+        foreach(var slot in bgSlots)
+        {
+            slot.Translate(scrollingSpeed * Time.deltaTime * -transform.right);
+            if( slot.position.x < edgePoint)
+            {
+                slot.Translate(width * bgSlots.Length * transform.right);
+            }
+        }
+    }
 }
