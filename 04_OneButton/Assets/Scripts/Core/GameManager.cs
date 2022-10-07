306c257193d936804a18292753ccb31d48b14f4b
go2665 <77224762+go2665@users.noreply.github.com>
2022-10-07 09:27:44 +0900

    OneButton - 랭킹 수정 중

--- 04_OneButton/Assets/Scripts/Core/GameManager.cs
+++ 04_OneButton/Assets/Scripts/Core/GameManager.cs
@@ -12,8 +12,7 @@ public class GameManager : Singleton<GameManager>
     PipeRotator pipeRotator;
 
     int score = 0;
-    int bestScore = 0;
-
+    
     const int RankCount = 5;
     int[] highScores = new int[RankCount];              //0번째 1등, 4번째 꼴등
     string[] highScorerNames = new string[RankCount];
@@ -34,19 +33,14 @@ public class GameManager : Singleton<GameManager>
         }
     }
 
-    public int BestScore
-    {
-        get => bestScore;
-        private set => bestScore = value;        
-    }
+    public int BestScore => highScores[0];
 
     public int[] HighScores => highScores;
     public string[] HighScorer => highScorerNames;
 
     protected override void Initialize()
     {
-        player = FindObjectOfType<Bird>();
-        player.onDead += BestScoreUpdate;       // 새가 죽을 때 최고 점수 갱신 시도
+        player = FindObjectOfType<Bird>();        
         player.onDead += RankUpdate;            // 새가 죽을 때 랭크 갱신
 
         pipeRotator = FindObjectOfType<PipeRotator>();
@@ -69,8 +63,8 @@ public class GameManager : Singleton<GameManager>
     {
         // Serializable로 되어 있는 클래스 만들기        
         SaveData saveData = new();              // 해당 클래스의 인스턴스 만들기
-        saveData.bestScore = BestScore;         // 인스턴스에 데이터 기록
-        saveData.name = "임시 이름";
+        saveData.highScores = highScores;         // 인스턴스에 데이터 기록
+        saveData.highScorerNames = new string[] { "임시 이름1", "임시 이름2", "임시 이름3", "임시 이름4", "임시 이름5"};
 
         string json = JsonUtility.ToJson(saveData);     // 해당 클래스를 json형식의 문자열로 변경
 
@@ -91,29 +85,29 @@ public class GameManager : Singleton<GameManager>
     {
         string path = $"{Application.dataPath}/Save/";      // 경로 확인용
         string fullPath = $"{path}Save.json";               // 전체 경로 확인용
-
-        BestScore = 0;  // 기본 값 설정
+                
         if (Directory.Exists(path) && File.Exists(fullPath)) // 해당 폴더가 있고 파일도 있으면
         {
             string json = File.ReadAllText(fullPath);       // json형식의 데이터 읽기
             SaveData loadData = JsonUtility.FromJson<SaveData>(json);   //SaveData 타입에 맞게 파싱
-            BestScore = loadData.bestScore;                 // 읽어온 데이터로 BestScore 변경
+            highScores = loadData.highScores;               // 읽어온 데이터로 최고점수 기록 변경
+            highScorerNames = loadData.highScorerNames;     // 이름들도 가져오기
         }
-    }
-
-    void BestScoreUpdate()
-    {
-        if(BestScore < Score)
+        else
         {
-            BestScore = Score;              // 점수 갱신
-            onBestScoreChange?.Invoke();    // 점수가 갱신되면 델리게이트에 연결된 함수들 실행
-            SaveGameData();                 // 갱신한 점수로 저장
+            highScores = new int[] { 0, 0, 0, 0, 0 };
+            highScorerNames = new string[] { "", "", "", "", "" };
         }
     }
 
     public void RankUpdate()
     {
-        for(int i=0;i<RankCount;i++)
+        if (BestScore < Score)
+        {
+            onBestScoreChange?.Invoke();    // 점수가 갱신되면 델리게이트에 연결된 함수들 실행(뉴마크 보이기)
+        }
+
+        for (int i=0;i<RankCount;i++)
         {
             if(highScores[i] < Score)   // 한 단계씩 비교해서 Score가 더 크면
             {
@@ -126,6 +120,7 @@ public class GameManager : Singleton<GameManager>
                 highScorerNames[i] = "";
 
                 onRankChange?.Invoke();
+                SaveGameData();                 // 갱신한 점수로 저장
                 break;
             }
         }        
--- 04_OneButton/Assets/Scripts/Core/SaveData.cs
+++ 04_OneButton/Assets/Scripts/Core/SaveData.cs
@@ -6,6 +6,6 @@ using UnityEngine;
 [Serializable]
 public class SaveData
 {
-    public int bestScore;
-    public string name;
+    public int[] highScores;            // 최고 득점들. 0번째가 1등, 4번째가 꼴등(5등)
+    public string[] highScorerNames;    // 최득점자 이름. 순서는 위와 같음
 }
