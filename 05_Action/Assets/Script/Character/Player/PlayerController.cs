a3632e433d9d1f1b2c9325632a8b7586f6ca2bf5
go2665 <77224762+go2665@users.noreply.github.com>
2022-10-13 11:56:19 +0900

    Action - 캐릭터 이동 처리 진행 중

--- 05_Action/Assets/Script/Character/Player/PlayerController.cs
+++ 05_Action/Assets/Script/Character/Player/PlayerController.cs
@@ -1,7 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 3.0f;
+    Vector3 inputDir = Vector3.zero;
+    Quaternion targetRotation = Quaternion.identity;
+
+    PlayerInputActions inputActions;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Move.canceled += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Move.canceled -= OnMove;
+        inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Disable();
+    }
+
+    private void Update()
+    {
+        transform.Translate(moveSpeed * Time.deltaTime * inputDir, Space.World);
+
+        transform.rotation = targetRotation;
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        Debug.Log(input);
+        inputDir.x = input.x;
+        inputDir.y = 0.0f;
+        inputDir.z = input.y;
+
+        Quaternion cameraYRotation = Quaternion.Euler(0, Camera.main.transform.rotation.eulerAngles.y, 0);
+        inputDir = cameraYRotation * inputDir;
+
+        targetRotation = Quaternion.LookRotation(inputDir);
+    }
 }
