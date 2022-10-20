d2dc9d6495e9834ca586c8f1b12f1dbab0b503fe
go2665 <77224762+go2665@users.noreply.github.com>
2022-10-20 14:17:22 +0900

    Action - 적의  HP Bar 빌보드로 만들기

--- 05_Action/Assets/Script/Character/Enemy_HP_Bar.cs
+++ 05_Action/Assets/Script/Character/Enemy_HP_Bar.cs
@@ -19,4 +19,13 @@ public class Enemy_HP_Bar : MonoBehaviour
     {
         fill.localScale = new Vector3(ratio, 1, 1); // 입력받은 비율대로 스케일 조절
     }
+
+    /// <summary>
+    /// 모든 게임 오브젝트의 Update 함수가 호출 된 이후에 호출되는 Update 함수.
+    /// </summary>
+    private void LateUpdate()
+    {
+        //transform.forward = Camera.main.transform.forward;  // 카메라의 forward와 나의 forward를 일치시킴
+        transform.rotation = Camera.main.transform.rotation;    // 카메라와 회전을 일치시킴
+    }
 }
