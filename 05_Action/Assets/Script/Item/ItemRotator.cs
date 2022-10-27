3ffb0374c33794afc82a9d44fe301435146efe40
go2665 <77224762+go2665@users.noreply.github.com>
2022-10-27 13:15:24 +0900

    Action - ItemRotator 실습

---
+++ 05_Action/Assets/Script/Item/ItemRotator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 이 스크립트를 가진 오브젝트는 y축을 기준으로 계속 회전(시계방향)하면서 위아래로 올라갔다 내려갔다 한다(삼각함수 활용).
+public class ItemRotator : MonoBehaviour
+{
+    public float rotateSpeed;   // 오브젝트의 회전 속도
+    public float minHeight;     // 오브젝트의 가장 낮은 높이
+    public float maxHeight;     // 오브젝트의 가장 높은 높이
+
+}
