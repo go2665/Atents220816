22310374b93103609349f0437a9cbca9e17ee99b
go2665 <77224762+go2665@users.noreply.github.com>
2022-11-28 11:57:42 +0900

    MineSweeper - ImageNumber 기본 설정

---
+++ 06_MineSweeper/Assets/Scripts/UI/ImageNumber.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageNumber : MonoBehaviour
+{
+    public Sprite[] numbers;
+
+    public int Number
+    {
+        get;
+        set;
+    }
+}
