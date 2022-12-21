4e82376a759907ea1f675f0405809cf116243016
go2665 <77224762+go2665@users.noreply.github.com>
2022-12-21 12:07:32 +0900

    TileMap - 슬라임용 쉐이더 그래프 완료

--- 07_TileMap/Assets/Scripts/Test/Test_Shader.cs
+++ 07_TileMap/Assets/Scripts/Test/Test_Shader.cs
@@ -7,7 +7,12 @@ public class Test_Shader : TestBase
 {
     public GameObject phaseSlime;
     public GameObject phaseReverseSlime;
+    public GameObject dessolveSlime;
+    public GameObject allSlime;
+
     public float phaseDuration = 2.0f;
+    public float dessolveDuration = 2.0f;
+    public float allDuration = 1.5f;
 
     protected override void Test1(InputAction.CallbackContext _)
     {
@@ -17,6 +22,16 @@ public class Test_Shader : TestBase
     protected override void Test2(InputAction.CallbackContext _)
     {
         StartCoroutine(StartPhase(phaseReverseSlime));
+    }
+
+    protected override void Test3(InputAction.CallbackContext _)
+    {
+        StartCoroutine(StartDessolve(dessolveSlime));
+    }
+
+    protected override void Test4(InputAction.CallbackContext _)
+    {
+        StartCoroutine(StartAllTest(allSlime));
     }
 
     IEnumerator StartPhase(GameObject target)
@@ -39,5 +54,46 @@ public class Test_Shader : TestBase
         }
 
         material.SetFloat("_Thickness", 0.0f);
+    }
+
+    IEnumerator StartDessolve(GameObject target)
+    {
+        Renderer renderer = target.GetComponent<SpriteRenderer>();
+        Material material = renderer.material;
+        material.SetFloat("_Fade", 1.0f);
+
+        float timeElipsed = 0.0f;
+        float dessolveDurationNormalize = 1 / dessolveDuration;
+
+        while (timeElipsed < dessolveDuration)
+        {
+            timeElipsed += Time.deltaTime;
+
+            material.SetFloat("_Fade", 1 - timeElipsed * dessolveDurationNormalize);
+
+            yield return null;
+        }
+    }
+
+    IEnumerator StartAllTest(GameObject target)
+    {
+        Renderer renderer = target.GetComponent<Renderer>();
+        Material material = renderer.material;
+
+        material.SetFloat("_Phase_Split", 0.0f);
+        material.SetFloat("_Dissolve_Fade", 1.0f);
+
+        float timeElipsed = 0.0f;
+        float allDurationNormalize = 1 / allDuration;
+
+        while (timeElipsed < dessolveDuration)
+        {
+            timeElipsed += Time.deltaTime;
+
+            material.SetFloat("_Phase_Split", timeElipsed * allDurationNormalize);
+            material.SetFloat("_Dissolve_Fade", 1 - timeElipsed * allDurationNormalize);
+
+            yield return null;
+        }
     }
 }
