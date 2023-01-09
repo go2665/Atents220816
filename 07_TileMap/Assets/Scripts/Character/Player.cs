fc03e01cc36c7984d71814893d120386e0d7fcc9
go2665 <77224762+go2665@users.noreply.github.com>
2023-01-09 09:51:57 +0900

    TileMap - 플레이어 수명 추가

--- 07_TileMap/Assets/Scripts/Character/Player.cs
+++ 07_TileMap/Assets/Scripts/Character/Player.cs
@@ -6,35 +6,68 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    // 수명 관련 변수들 ---------------------------------------------------------------------------
+
+    /// <summary>
+    /// 플레이어의 최대 수명
+    /// </summary>
+    public float maxLifeTime = 10.0f;
+
+    /// <summary>
+    /// 플레이어의 현재 수명
+    /// </summary>
+    float lifeTime;
+
+    /// <summary>
+    /// 전체 플레이 시간
+    /// </summary>
+    float totalPlayTime;
+
+    /// <summary>
+    /// 플레이어의 생존 여부 표시용
+    /// </summary>
+    bool isDead = false;
+
+    /// <summary>
+    /// 플레이어의 수명을 확인하거나 설정할 때의 여러 처리를 하는 프로퍼티
+    /// </summary>
+    public float LifeTime
+    {
+        get => lifeTime;
+        set 
+        {
+            lifeTime = value;                   // 우선 값 변경
+            if(lifeTime < 0.0f && !isDead)      // 아직 안죽었는데 수명이 0 이하면
+            {
+                Die();                          // 사망 처리
+                onLifeTimeChange?.Invoke(lifeTime, maxLifeTime);    // 수명이 변경되었다고 알림(죽은 직후에 한번만 실행)
+            }
+            else
+            {
+                lifeTime = Math.Clamp(value, 0.0f, maxLifeTime);    // 아니면 수명을 (0~최대값)으로 설정
+                onLifeTimeChange?.Invoke(lifeTime, maxLifeTime);    // 수명이 변경되었다고 알림
+            }
+        }
+    }
+
+    /// <summary>
+    /// 플레이어의 수명이 변경되었을 때 실행될 델리게이트
+    /// 비네트, 슬라이더, 남은 시간 표시용으로 사용
+    /// </summary>
+    public Action<float, float> onLifeTimeChange;
+
+    /// <summary>
+    /// 플레이어가 죽었을 때 실행될 델리게이트
+    /// </summary>
+    public Action onDie;
+
+
+    // 이동 관련 변수들 ---------------------------------------------------------------------------
+    
     /// <summary>
     /// 플레이어 이동 속도
     /// </summary>
     public float speed = 3.0f;
-
-    /// <summary>
-    /// 플레이어의 공격 쿨타임
-    /// </summary>
-    public float attackCoolTime = 1.0f;
-
-    /// <summary>
-    /// 플레이어의 현재 남아있는 쿨타임
-    /// </summary>
-    float currentAttackCoolTime = 0.0f;
-
-    /// <summary>
-    /// 애니메이터 컴포넌트
-    /// </summary>
-    Animator anim;
-
-    /// <summary>
-    /// 리지드바디 컴포넌트
-    /// </summary>
-    Rigidbody2D rigid;
-
-    /// <summary>
-    /// 인풋 시스템용 인풋 액션맵 클래스 객체
-    /// </summary>
-    PlayerInputActions inputActions;
 
     /// <summary>
     /// 현재 입력된 이동 방향
@@ -50,21 +83,6 @@ public class Player : MonoBehaviour
     /// 현재 이동 중인지 표시하는 변수
     /// </summary>
     bool isMove = false;
-
-    /// <summary>
-    /// 공격 영역의 중심(축)
-    /// </summary>
-    Transform attackAreaCenter;
-
-    /// <summary>
-    /// 플레이어가 공격하면 죽을 슬라임들
-    /// </summary>
-    List<Slime> attackTarget;
-
-    /// <summary>
-    /// 공격유효기간 표시. true면 슬라임을 죽일 수 있다. false면 못 죽이는 상황
-    /// </summary>
-    bool isAttackValid = false;
 
     /// <summary>
     /// 맵 매니저
@@ -84,7 +102,7 @@ public class Player : MonoBehaviour
         get => currentMap;
         set
         {
-            if( currentMap != value)            // 맵이 변경이 될 때만
+            if (currentMap != value)            // 맵이 변경이 될 때만
             {
                 currentMap = value;             // 실제로 변경
                 onMapMoved?.Invoke(currentMap); // 델리게이트로 변경되었음을 알림
@@ -96,6 +114,53 @@ public class Player : MonoBehaviour
     /// 맵 변경시 실행될 델리게이트(파라메터는 변경된 맵의 그리드 좌표)
     /// </summary>
     public Action<Vector2Int> onMapMoved;
+
+    // 공격 관련 변수들 ---------------------------------------------------------------------------
+
+    /// <summary>
+    /// 플레이어의 공격 쿨타임
+    /// </summary>
+    public float attackCoolTime = 1.0f;
+
+    /// <summary>
+    /// 플레이어의 현재 남아있는 쿨타임
+    /// </summary>
+    float currentAttackCoolTime = 0.0f;
+
+    /// <summary>
+    /// 공격 영역의 중심(축)
+    /// </summary>
+    Transform attackAreaCenter;
+
+    /// <summary>
+    /// 플레이어가 공격하면 죽을 슬라임들
+    /// </summary>
+    List<Slime> attackTarget;
+
+    /// <summary>
+    /// 공격유효기간 표시. true면 슬라임을 죽일 수 있다. false면 못 죽이는 상황
+    /// </summary>
+    bool isAttackValid = false;
+
+    // 기타 변수들 --------------------------------------------------------------------------------
+
+    /// <summary>
+    /// 애니메이터 컴포넌트
+    /// </summary>
+    Animator anim;
+
+    /// <summary>
+    /// 리지드바디 컴포넌트
+    /// </summary>
+    Rigidbody2D rigid;
+
+    /// <summary>
+    /// 인풋 시스템용 인풋 액션맵 클래스 객체
+    /// </summary>
+    PlayerInputActions inputActions;
+
+    
+    // 함수들 -------------------------------------------------------------------------------------
 
     private void Awake()
     {
@@ -149,7 +214,10 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        // 아무 조건 없이 계속 쿨타임 감소
+        totalPlayTime += Time.deltaTime;
+        LifeTime -= Time.deltaTime;
+
+        // 아무 조건 없이 계속 공격 쿨타임 감소
         currentAttackCoolTime -= Time.deltaTime;    
 
         if(isAttackValid)
@@ -251,5 +319,15 @@ public class Player : MonoBehaviour
     public void AttackNotValid()
     {
         isAttackValid = false;
+    }
+
+    /// <summary>
+    /// 타임오버로 사망시 실행될 함수
+    /// </summary>
+    void Die()
+    {
+        lifeTime = 0.0f;    // 비네트, 슬라이더, 남은 시간을 깔끔하게 표시하기 위해 0으로 설정
+        isDead = true;      // 여러번 호출되지 않도록하기 위치 설정
+        onDie?.Invoke();    // 죽었다고 알리기
     }
 }
