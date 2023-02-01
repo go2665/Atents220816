c42381b4dbfa2953c4862bad1ff721a5913fe163
go2665 <77224762+go2665@users.noreply.github.com>
2023-02-01 14:22:12 +0900

    BoardGame - Board 클래스의 유틸리티 함수 작성

--- 09_BoardGame/Assets/Scripts/Board/Board.cs
+++ 09_BoardGame/Assets/Scripts/Board/Board.cs
@@ -17,21 +17,32 @@ public class Board : MonoBehaviour
     /// <summary>
     /// 배열의 인덱스 값을 그리드 좌표로 변환해주는 static 함수
     /// </summary>
-    /// <param name="index"></param>
-    /// <returns></returns>
+    /// <param name="index">계산할 인덱스 값</param>
+    /// <returns>변환된 그리드 좌표</returns>
     static public Vector2Int IndexToGrid(int index)
     {
-        return Vector2Int.zero;
+        return new Vector2Int(index % BoardSize, index / BoardSize);
     }
 
     /// <summary>
     /// 그리드 좌표를 배열의 인덱스 값으로 변환해주는 static 함수
     /// </summary>
-    /// <param name="grid"></param>
-    /// <returns></returns>
+    /// <param name="grid">계산할 그리드 좌표</param>
+    /// <returns>변환된 인덱스 값</returns>
     static public int GridToIndex(Vector2Int grid)
     {
-        return 0;
+        return grid.x + grid.y * BoardSize;
+    }
+
+    /// <summary>
+    /// 그리드 좌표를 배열의 인덱스 값으로 변환해주는 static 함수
+    /// </summary>
+    /// <param name="x">계산할 그리드 x좌표</param>
+    /// <param name="y">계산할 그리드 y좌표</param>
+    /// <returns>변환된 인덱스 값</returns>
+    static public int GridToIndex(int x, int y)
+    {
+        return x + y * BoardSize;
     }
 
     /// <summary>
@@ -42,7 +53,7 @@ public class Board : MonoBehaviour
     /// <returns></returns>
     public Vector3 GridToWorld(int x, int y)
     {
-        return Vector3.zero;
+        return transform.position + new Vector3(x + 0.5f, 0, -(y + 0.5f));
     }
 
     /// <summary>
@@ -62,7 +73,11 @@ public class Board : MonoBehaviour
     /// <returns></returns>
     public Vector2Int WorldToGrid(Vector3 worldPos)
     {
-        return Vector2Int.zero;
+        worldPos.y = 0;
+
+        Vector3 diff = worldPos - transform.position;
+
+        return new Vector2Int((int)diff.x, (int)-diff.z);
     }
 
     /// <summary>
--- 09_BoardGame/Assets/Scripts/Test/Test_Board.cs
+++ 09_BoardGame/Assets/Scripts/Test/Test_Board.cs
@@ -29,6 +29,56 @@ public class Test_Board : TestBase
     {
         Vector2 screen = Mouse.current.position.ReadValue();
         Vector3 world = Camera.main.ScreenToWorldPoint(screen);
-        Debug.Log(world);
+        Vector2Int grid = board.WorldToGrid(world);
+        Debug.Log($"클릭 그리드 : ({grid.x}, {grid.y})");
+        Vector3 GtoW = board.GridToWorld(grid);
+        Debug.Log($"클릭 월드 : ({GtoW.x}, {GtoW.z})");
+    }
+
+    protected override void Test1(InputAction.CallbackContext _)
+    {
+        if( Board.GridToIndex(1,1) == 11 
+            && Board.GridToIndex(1, 2) == 21
+            && Board.GridToIndex(0, 9) == 90)
+        {
+            Debug.Log("정상 - GridToIndex");
+        }
+        else
+        {
+            Debug.LogError("비정상 - GridToIndex");
+        }
+        
+        if( Board.IndexToGrid(11) == new Vector2Int(1,1)
+            && Board.IndexToGrid(21) == new Vector2Int(1, 2)
+            && Board.IndexToGrid(90) == new Vector2Int(0, 9) )
+        {
+            Debug.Log("정상 - IndexToGrid");
+        }
+        else
+        {
+            Debug.LogError("비정상 - IndexToGrid");
+        }
+
+        if( board.WorldToGrid(board.transform.position + new Vector3(3.5f, 0, -2.1f)) == new Vector2Int(3,2) 
+            && board.WorldToGrid(board.transform.position + new Vector3(1.5f, 0, -7.1f)) == new Vector2Int(1, 7)
+            && board.WorldToGrid(board.transform.position + new Vector3(5.9f, 0, -3.1f)) == new Vector2Int(5, 3))
+        {
+            Debug.Log("정상 - WorldToGrid");
+        }
+        else
+        {
+            Debug.LogError("비정상 - WorldToGrid");
+        }
+
+        if( board.GridToWorld(0,0) == new Vector3(board.transform.position.x + 0.5f, board.transform.position.y, board.transform.position.z-0.5f)
+            && board.GridToWorld(1, 1) == new Vector3(board.transform.position.x + 0.5f + 1, board.transform.position.y, board.transform.position.z - 0.5f -1)
+            && board.GridToWorld(5, 3) == new Vector3(board.transform.position.x + 0.5f + 5, board.transform.position.y, board.transform.position.z - 0.5f -3))
+        {
+            Debug.Log("정상 - GridToWorld");
+        }
+        else
+        {
+            Debug.LogError("비정상 - GridToWorld");
+        }
     }
 }
