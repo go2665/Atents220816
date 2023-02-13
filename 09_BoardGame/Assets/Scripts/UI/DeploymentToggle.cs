5b67d94fc86f228b78298b242e86a88e918f95ef
go2665 <77224762+go2665@users.noreply.github.com>
2023-02-13 12:51:03 +0900

    BoardGame - 함선 배치 패널(토글 버튼 완료)

--- 09_BoardGame/Assets/Scripts/UI/DeploymentToggle.cs
+++ 09_BoardGame/Assets/Scripts/UI/DeploymentToggle.cs
@@ -6,28 +6,54 @@ using UnityEngine.UI;
 
 public class DeploymentToggle : MonoBehaviour
 {
+    /// <summary>
+    /// 버튼의 이미지
+    /// </summary>
     Image image;
+
+    /// <summary>
+    /// 이 스크립트가 있는 버튼
+    /// </summary>
     Button button;
 
+    /// <summary>
+    /// 눌러졌을 때 적용될 색상
+    /// </summary>
     readonly Color selectedColor = new(1, 1, 1, 0.2f);
 
-    bool isDeployed = false;
-    private bool IsDeployed
+    /// <summary>
+    /// 버튼이 눌려진 상태인지 여부
+    /// </summary>
+    bool isToggled = false;
+
+    /// <summary>
+    /// 버튼 토글 상태를 확인하거나 토글 되었을 때 수행할 일들을 처리하는 프로퍼티
+    /// </summary>
+    private bool IsToggled
     {
-        get => isDeployed;
+        get => isToggled;
         set
         {
-            isDeployed = value;
-            if( isDeployed )
+            if( isToggled != value )                // 토글 상태가 변경되는지 확인
             {
-                image.color = selectedColor;
-            }
-            else
-            {
-                image.color = Color.white;
+                isToggled = value;                  // 변경된 상황이면 변경 적용
+                if( isToggled )
+                {
+                    image.color = selectedColor;    // 눌려졌으면 색상 변경하고
+                    onTogglePress?.Invoke(this);    // 눌려졌다고 델리게이트 날리기
+                }
+                else
+                {
+                    image.color = Color.white;      // 눌려진 것이 해제되었으면 원래 색상으로 되돌리기
+                }
             }
         }
     }
+
+    /// <summary>
+    /// 눌려지지 않은 상태에서 눌러졌을 때 실행될 델리게이트
+    /// </summary>
+    public Action<DeploymentToggle> onTogglePress;    
 
     private void Awake()
     {
@@ -36,8 +62,19 @@ public class DeploymentToggle : MonoBehaviour
         button.onClick.AddListener(OnClick);
     }
 
+    /// <summary>
+    /// 버튼이 클릭되었을 때 실행되는 함수
+    /// </summary>
     private void OnClick()
     {
-        IsDeployed = !IsDeployed;
+        IsToggled = !IsToggled; // 토글 상태만 반전 시킴
+    }
+
+    /// <summary>
+    /// 눌려진 상태를 헤제하는 함수
+    /// </summary>
+    public void UnToggle()
+    {
+        IsToggled = false;      // 무조건 해제
     }
 }
--- 09_BoardGame/Assets/Scripts/UI/ShipDeploymentPanel.cs
+++ 09_BoardGame/Assets/Scripts/UI/ShipDeploymentPanel.cs
@@ -1,8 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipDeploymentPanel : MonoBehaviour
 {
-    
+    /// <summary>
+    /// 이 패널 아래에 있는 모든 토글 버튼
+    /// </summary>
+    DeploymentToggle[] toggles = null;
+
+    private void Awake()
+    {
+        toggles = GetComponentsInChildren<DeploymentToggle>();
+        foreach (DeploymentToggle toggle in toggles)
+        {
+            toggle.onTogglePress += OnTogglePress;  // 모든 토글 버튼의 델리게이트에 함수 등록
+        }
+    }
+
+    /// <summary>
+    /// 토글 버튼이 눌러진 상태가 되었을 때 실행되는 함수
+    /// </summary>
+    /// <param name="toggle">눌러진 토글 버튼</param>
+    private void OnTogglePress(DeploymentToggle toggle)
+    {
+        foreach(var others in toggles)  
+        {
+            if( toggle != others )  // 자신을 제외한 나머지 토글 버튼들을
+            {
+                others.UnToggle();  // 눌러진 상태 해제
+            }
+        }
+    }
 }
